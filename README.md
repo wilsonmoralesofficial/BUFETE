b0a4260e72700ae993e264afe6c3fd98e0756146
wilink-king123 <wilsonmoralesofficial@gmail.com>
2021-09-29 22:13:33 -0600

    v1.0

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# BUFETE
+=======
+# BUFETE
+Bufete juridico Argueta&amp;Asociados
+>>>>>>> d6495f74eb7750f0ae255378e7357802cc21bd21
---
+++ index.js
@@ -0,0 +1,14 @@
+//Agregamos la libreria Muuri al contenedor que contiene la clase .grid
+const grid = new Muuri('.grid',{
+    layout: {
+        rounding: false 
+    }
+});
+
+//Agregamos los listeners de los enlaces para seleccionar 
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    
+    
+    document.getElementById('grid').classList.add('refresh')
+});
